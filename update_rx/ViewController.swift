2eb76a263222a005fd608162f3f363e3e34557e3
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2023-12-06 22:13:25 +0900

    Initial Commit

---
+++ update_rx/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  update_rx
+//
+//  Created by 菅原大輝 on 2023/12/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
