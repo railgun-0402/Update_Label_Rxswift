ffdc1d256f614f507d7bb280588fcab4f5876ef8
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2023-12-06 23:50:08 +0900

    create update func

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'update_rx' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for update_rx
+  pod 'RxSwift', '~> 6'
+  pod 'RxCocoa', '~> 6'
+
+end
--- update_rx/ViewController.swift
+++ update_rx/ViewController.swift
@@ -2,18 +2,34 @@
 //  ViewController.swift
 //  update_rx
 //
-//  Created by 菅原大輝 on 2023/12/06.
-//
-
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    @IBOutlet weak var updateButton: UIButton!
+    @IBOutlet weak var inputTextField: UITextField!
+    
+    private let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // テキストフィールドの入力を監視し、変更があれば変更内容を表示ラベルに反映
+        inputTextField.rx.text
+            .map{ $0 ?? ""}
+            .bind(to: displayLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        // 更新ボタンがタップされた時の処理
+        updateButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                // ボタンが押されたら、テキストフィールドの内容をラベルに反映
+                self?.displayLabel.text = self?.inputTextField.text
+            })
+            .disposed(by: disposeBag)
     }
-
-
 }
 
