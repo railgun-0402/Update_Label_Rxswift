d0708353ef4ce2a9d059a4438f73a1960d32595c
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2023-12-06 23:50:44 +0900

    del comment

--- update_rx/AppDelegate.swift
+++ update_rx/AppDelegate.swift
@@ -1,9 +1,6 @@
 //
 //  AppDelegate.swift
 //  update_rx
-//
-//  Created by 菅原大輝 on 2023/12/06.
-//
 
 import UIKit
 
--- update_rx/SceneDelegate.swift
+++ update_rx/SceneDelegate.swift
@@ -2,8 +2,6 @@
 //  SceneDelegate.swift
 //  update_rx
 //
-//  Created by 菅原大輝 on 2023/12/06.
-//
 
 import UIKit
 
